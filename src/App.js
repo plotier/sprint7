4e8c2dfead58c0a7bcecb06e65c8051bd545e9a4
plotier <plotier15@gmail.com>
2022-01-14 19:47:35 +0100

    finally, second exercice done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
-
+import { Panell } from './styled';
 function App() {
 
+  //Array con los precios de los input
   const [totalPrice, setTotalPrice] = useState([]);
+
+  //Precio de las paginas e idiomas en el apartado web
+  const[extraWeb, setExtraWeb]=useState({
+    paginas:1,
+    idiomas:1
+  })
+
+  //Suma total de todos los precios
   const [sumaPrecio, setSumaPrecio] = useState(0)
+  const [plusWeb, setPlusWeb] = useState(1)
+
+
   const [isCheckedOne, setIsCheckedOne] = useState(false);
   const [isCheckedTwo, setIsCheckedTwo] = useState(false);
   const [isCheckedThree, setIsCheckedThree] = useState(false);
+
   const handleOnChangeOne = e => {
     setIsCheckedOne(!isCheckedOne);
     e.target.checked ? setTotalPrice([...totalPrice, parseInt(e.target.value)])
@@ -28,11 +41,33 @@ function App() {
       setTotalPrice(totalPrice => totalPrice.filter(item => item !== parseInt(e.target.value)))
   };
 
+  //Funcion para calcular precio web con numero de paginas e idiomas
+  // Numero de pàgines * el numero d'idiomes * 30 €.
+  const handlePagNum=(event)=>{
+    let value = parseInt(event.target.value);
+    let newValue = (Number.isNaN(value) ? 1 : value);
+    setExtraWeb(
+      {
+      ...extraWeb,
+      [event.target.name]:newValue}
+    )      
+  }
+
+//Suma total de todos los precios
   useEffect(() => {
     if (totalPrice.length !== 0) {
       setSumaPrecio(totalPrice.reduce((a, b) => a + b))
     }
   })
+  useEffect(() => {
+    let cantPaginas = extraWeb.paginas
+    let cantIdiomas = extraWeb.idiomas 
+    let resultado =  cantIdiomas*cantPaginas*30
+    resultado !=30?resultado=resultado:resultado=0;
+    setPlusWeb(
+      resultado
+    )},[extraWeb])
+
 
   return (
     <div className='container m-5'>
@@ -42,6 +77,22 @@ function App() {
           Una página web (€500)
         </label>
       </div>
+
+      {/*////Renderizado condicional*/}
+    {isCheckedOne && 
+          <Panell>
+          <form className='d-flex flex-column'>
+            <label htmlFor="paginas" className='m-2'>Número de páginas:
+              <input type='number' name='paginas' className='costumeInput' onChange={handlePagNum} placeholder="1"></input>
+            </label>
+            <label htmlFor="idiomas" className='m-2'>Número de idiomas:
+              <input type='number' name='idiomas' className='costumeInput' onChange={handlePagNum} placeholder="1"></input>
+            </label>
+          </form>
+        </Panell>
+    }
+
+
       <div className="form-check">
         <input className="form-check-input" type="checkbox" value="300" id="SEO" onChange={e => handleOnChangeTwo(e)} checked={isCheckedTwo} />
         <label className="form-check-label" htmlFor="flexCheckDefault">
@@ -55,7 +106,7 @@ function App() {
         </label>
       </div>
       <div>
-        <h5>Total : {sumaPrecio}</h5>
+        <h5>Total : {sumaPrecio+plusWeb} </h5>
       </div>
 
     </div>
---
+++ src/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Panell = styled.div`
+border:5px solid;
+margin:10px;
+padding:10px;
+display: inline-block;
+border-radius:10px
+
+`;
