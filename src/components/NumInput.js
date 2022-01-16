a8e75d229bf80c983004702cb680694429f5efda
plotier <plotier15@gmail.com>
2022-01-16 15:51:08 +0100

    Exercici 3, done!

---
+++ src/components/NumInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NumInput = props =>{
+
+return(
+    <input type='number'ref={props.referencia} value={props.valor} min="1" name={props.name} className='costumeInput' onChange={props.funcion} ></input>
+)
+
+}
