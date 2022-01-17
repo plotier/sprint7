fa72007d929544702294f4dad46214101d52c7b9
plotier <plotier15@gmail.com>
2022-01-17 15:25:36 +0100

    exercici 4, local storage done!

--- src/components/NumInput.js
+++ src/components/NumInput.js
@@ -5,5 +5,4 @@ export const NumInput = props =>{
 return(
     <input type='number'ref={props.referencia} value={props.valor} min="1" name={props.name} className='costumeInput' onChange={props.funcion} ></input>
 )
-
 }
