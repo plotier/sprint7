8c768d519b69b4a7ecad462ed863f7414e72b79a
plotier <plotier15@gmail.com>
2022-01-17 23:23:47 +0100

    exercici 5 terminado

---
+++ src/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export const Welcome = () => {
+    return (
+        <div className="m-5">
+            <h1>Welcome page</h1>
+            <Link to="/home">
+            <button type="button" class="btn btn-primary"> Home</button>
+            </Link>
+        </div>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Home } from './Home';
+import { Welcome } from './Welcome';
+
+export default () => (
+    <BrowserRouter>
+        <Routes>   
+            <Route path="/" element={<Welcome/>} />
+            <Route path="/home/" element={<Home/>} />
+             { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}    
+            <Route path="*" element={<div>404</div> } />
+        </Routes>
+    </BrowserRouter>
+);
